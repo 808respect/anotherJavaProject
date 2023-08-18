1c00b90054d5050b542666f3c8dde9f4ea41d5e3
REESCOH <respect.r.tsvuura@gmail.com>
2023-08-18 11:40:55 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+// Press ⇧ twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Contact.contacts.add(new Contact("tinashe@gmail.com", "12346578"));
+        Contact.contacts.add(new Contact("chipo@gmail.com", "12346578"));
+        Contact.contacts.add(new Contact("tendai@gmail.com", "12346578"));
+        Contact.contacts.add(new Contact("sekai@gmail.com", "12346578"));
+        Contact.contacts.add(new Contact("tinashe@gmail.com", "12346578"));
+        System.out.println("...Welcome to my contact Manager...");
+        System.out.println(" You have " + Contact.contacts.size() + " contacts in your list ..");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        int a = 34;
+//        int b = 32;
+//
+//
+//        if (a > b){
+//            System.out.println("a muhombe");
+//        };
+//
+//        boolean isTrue = (a > b);
+//        System.out.println(isTrue);
+//
+//       boolean numberEquater = a == b ? true : false;
+//       System.out.println(numberEquater);
+    };
+};
